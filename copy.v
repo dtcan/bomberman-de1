e06e90db68409d5dde97249dd54d0c6afc5d0abf
David Canagasabey <david.canagasabey@mail.utoronto.ca>
2018-11-21 16:32:21 -0500

    Updated copy.v, now works in 320x240

--- copy.v
+++ copy.v
@@ -1,18 +1,21 @@
 module copy(clk, reset_n, go, memory_select, tile_select, colour, offset, write_en, finished);
 	input clk, reset_n, go;
 	input [1:0] memory_select;
-	input [4:0] tile_select;
+	input [3:0] tile_select;
 	output reg write_en, finished;
 	output [14:0] colour;
-	output [18:0] offset;
+	output [16:0] offset;
+	
+	localparam WIDTH = 320, HEIGHT = 240; // Still have to go change bit-widths when changing these!
 	
 	wire [14:0] colour_1, colour_2, colour_3, colour_t;
-	wire [18:0] offset_s;
-	wire [9:0] offset_t;
+	wire [8:0] offset_x;
+	wire [7:0] offset_y;
+	wire [7:0] offset_t;
 	reg enable_count;
-	reg [18:0] adr;
+	reg [16:0] adr;
 	
-	count10 c0(
+	count8 c0(
 		.clk(clk),
 		.reset_n(reset_n),
 		.enable(enable_count & (memory_select == 2'b11)),
@@ -23,8 +26,8 @@ module copy(clk, reset_n, go, memory_select, tile_select, colour, offset, write_
 		.clk(clk),
 		.reset_n(reset_n),
 		.enable(enable_count & (memory_select != 2'b11)),
-		.q_x(offset_s[9:0]),
-		.q_y(offset_s[18:10])
+		.q_x(offset_x),
+		.q_y(offset_y)
 	);
 	
 	always @(*)
@@ -39,7 +42,7 @@ module copy(clk, reset_n, go, memory_select, tile_select, colour, offset, write_
 		if(memory_select == 2'b11)
 			offset = {9'd0, offset_t}
 		else
-			offset = offset_s
+			offset = {offset_y, offset_x}
 	end
 	
 	altsyncram	TitleScreen (
@@ -53,8 +56,8 @@ module copy(clk, reset_n, go, memory_select, tile_select, colour, offset, write_
 		TitleScreen.WIDTH_A = 5*3,
 		TitleScreen.INTENDED_DEVICE_FAMILY = "Cyclone II",
 		TitleScreen.OPERATION_MODE = "ROM",
-		TitleScreen.WIDTHAD_A = 19,
-		TitleScreen.NUMWORDS_A = 640 * 480,
+		TitleScreen.WIDTHAD_A = 17,
+		TitleScreen.NUMWORDS_A = WIDTH * HEIGHT,
 		TitleScreen.CLOCK_ENABLE_INPUT_A = "BYPASS",
 		TitleScreen.POWER_UP_UNINITIALIZED = "FALSE",
 		TitleScreen.INIT_FILE = "title.mif";
@@ -70,8 +73,8 @@ module copy(clk, reset_n, go, memory_select, tile_select, colour, offset, write_
 		GameScreen.WIDTH_A = 5*3,
 		GameScreen.INTENDED_DEVICE_FAMILY = "Cyclone II",
 		GameScreen.OPERATION_MODE = "ROM",
-		GameScreen.WIDTHAD_A = 19,
-		GameScreen.NUMWORDS_A = 640 * 480,
+		GameScreen.WIDTHAD_A = 17,
+		GameScreen.NUMWORDS_A = WIDTH * HEIGHT,
 		GameScreen.CLOCK_ENABLE_INPUT_A = "BYPASS",
 		GameScreen.POWER_UP_UNINITIALIZED = "FALSE",
 		GameScreen.INIT_FILE = "game.mif";
@@ -87,8 +90,8 @@ module copy(clk, reset_n, go, memory_select, tile_select, colour, offset, write_
 		EndScreen.WIDTH_A = 5*3,
 		EndScreen.INTENDED_DEVICE_FAMILY = "Cyclone II",
 		EndScreen.OPERATION_MODE = "ROM",
-		EndScreen.WIDTHAD_A = 19,
-		EndScreen.NUMWORDS_A = 640 * 480,
+		EndScreen.WIDTHAD_A = 17,
+		EndScreen.NUMWORDS_A = WIDTH * HEIGHT,
 		EndScreen.CLOCK_ENABLE_INPUT_A = "BYPASS",
 		EndScreen.POWER_UP_UNINITIALIZED = "FALSE",
 		EndScreen.INIT_FILE = "endscreen.mif";
@@ -97,18 +100,18 @@ module copy(clk, reset_n, go, memory_select, tile_select, colour, offset, write_
 				.wren_a (1'b0),
 				.clock0 (clk), // read clock
 				.clocken0 (1'b1), // read enable clock
-				.address_a (adr[13:0]),
+				.address_a (adr[11:0]),
 				.q_a (colour_t)	// data out
 				);
 	defparam
 		TileSet.WIDTH_A = 5*3,
 		TileSet.INTENDED_DEVICE_FAMILY = "Cyclone II",
 		TileSet.OPERATION_MODE = "ROM",
-		TileSet.WIDTHAD_A = 14,
-		TileSet.NUMWORDS_A = 32 * 32 * 16,
+		TileSet.WIDTHAD_A = 12,
+		TileSet.NUMWORDS_A = 16 * 16 * 16,
 		TileSet.CLOCK_ENABLE_INPUT_A = "BYPASS",
 		TileSet.POWER_UP_UNINITIALIZED = "FALSE",
-		TileSet.INIT_FILE = "endscreen.mif";
+		TileSet.INIT_FILE = "tileset.mif";
 
 	reg [3:0] Q, Qn;
 	localparam S_RESET     = 3'b000,
@@ -145,9 +148,9 @@ module copy(clk, reset_n, go, memory_select, tile_select, colour, offset, write_
 			S_SELECT:
 			begin
 				if(memory_select == 2'b11)
-					adr = {tile_select[1:0] * 32, tile_select[3:2] * 32} + offset;
+					adr = ({tile_select[3:2],offset[7:4]} * WIDTH) + {tile_select[1:0], offset[3:0]}
 				else
-					adr = (offset[9:0] * 640) + offset[18:10]
+					adr = (offset_y * WIDTH) + offset_x
 			end
 			S_DRAW: write_en = 1;
 			S_INCREMENT: enable_count = 1;
@@ -164,10 +167,10 @@ module copy(clk, reset_n, go, memory_select, tile_select, colour, offset, write_
 	end
 endmodule
 
-module count10(clk, reset_n, enable, q);
+module count8(clk, reset_n, enable, q);
 	input clk, reset_n, enable;
 	
-	output reg [9:0] q;
+	output reg [7:0] q;
 	always @(posedge clk)
 	begin
 		if(reset_n)
@@ -179,11 +182,11 @@ endmodule
 
 module count_xy(clk, reset_n, enable, max_x, max_y, q_x, q_y);
 	input clk, reset_n, enable;
-	input [9:0] max_x;
-	input [8:0] max_y;
+	input [8:0] max_x;
+	input [7:0] max_y;
 	
-	output reg [9:0] q_x;
-	output reg [8:0] q_y;
+	output reg [8:0] q_x;
+	output reg [7:0] q_y;
 	always @(posedge clk)
 	begin
 		if(reset_n)
@@ -194,12 +197,12 @@ module count_xy(clk, reset_n, enable, max_x, max_y, q_x, q_y);
 		else if(enable)
 		begin
 			q_x <= q_x + 1;
-			if(q_x == 640)
+			if(q_x == WIDTH)
 				q_x <= 0;
 			if(q_x == 0)
 			begin
 				q_y <= q_y + 1;
-				if(q_y == 480)
+				if(q_y == HEIGHT)
 					q_y <= 0;
 			end
 		end
