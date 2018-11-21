2cd088386c6670cd5057ab1acacd8c84ed95ac66
Andrew Chew <andy.chew@mail.utoronto.ca>
2018-11-21 12:44:54 -0500

    Shortened variable names for readability and removed redundant code.

--- keyboard_decoder.v
+++ keyboard_decoder.v
@@ -2,11 +2,11 @@
 // module that inteprets the keyboard input from the user for use in bomberman.v.
 
 module keyboard_decoder(
-	output reg player1_bomb, player2_bomb,				// 0: bomb NOT placed,					1: bomb placed.
-	output reg player1_xdir, player2_xdir,				// 0: moving LEFT in x direction, 	1: moving RIGHT in x direction.
-	output reg player1_ydir, player2_ydir,				// 0: moving UP in y direction,		1: moving DOWN in y direction.
-	output reg player1_x_moving, player2_x_moving,	// 0: NOT moving in x direction, 	1: moving in x direction.
-	output reg player1_y_moving, player2_y_moving,	// 0: NOT moving in y direction, 	1: moving in y direction.
+	output reg p1_bomb, p2_bomb,			// 0: bomb NOT placed,					1: bomb placed.
+	output reg p1_xdir, p2_xdir,			// 0: moving LEFT in x direction, 	1: moving RIGHT in x direction.
+	output reg p1_ydir, p2_ydir,			// 0: moving UP in y direction,		1: moving DOWN in y direction.
+	output reg p1_x_moving, p2_x_moving,// 0: NOT moving in x direction, 	1: moving in x direction.
+	output reg p1_y_moving, p2_y_moving,// 0: NOT moving in y direction, 	1: moving in y direction.
 	
 	inout PS2_CLK, PS2_DAT,
 	
@@ -15,10 +15,6 @@ module keyboard_decoder(
 	
 	wire w, a, s, d, space;
 	wire up, left, down, right, enter;
-	
-	// store user input into respective registers.
-	wire [3:0] player1_wasd, player2_udlr;
-	wire space, enter;
 	
 	// module for tracking keyboard input.
 	keyboard_tracker #(.PULSE_OR_HOLD(0)) kb(
@@ -42,18 +38,18 @@ module keyboard_decoder(
 	always @(*)
 		begin
 			// for player 1.
-			player1_bomb		<= space;
-			player1_x_dir 		<= d ? d : a;
-			player1_x_moving 	<= (a | d);
-			player1_y_dir		<= s ? s : w; 
-			player1_y_moving 	<=	(w | s);
+			p1_bomb		<= space;
+			p1_x_dir 	<= d ? d : a;
+			p1_x_moving <= (a | d);
+			p1_y_dir		<= s ? s : w; 
+			p1_y_moving <=	(w | s);
 			
 			// for player 2.
-			player2_bomb		<= enter;
-			player2_x_dir 		<= right ? right : left;
-			player2_x_moving 	<= (left | right);
-			player2_y_dir		<= down ? down : up;
-			player2_y_moving 	<= (up | down);
+			p2_bomb		<= enter;
+			p2_x_dir 	<= right ? right : left;
+			p2_x_moving <= (left | right);
+			p2_y_dir		<= down ? down : up;
+			p2_y_moving <= (up | down);
 		end
 
 endmodule
