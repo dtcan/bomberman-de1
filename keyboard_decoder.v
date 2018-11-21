aa3ed40fd3f38cfbb071e911ef21f0f3241d18f6
Andrew Chew <andy.chew@mail.utoronto.ca>
2018-11-21 12:47:34 -0500

    Shortened variable names for readability and removed redundant code.

--- keyboard_decoder.v
+++ keyboard_decoder.v
@@ -2,11 +2,11 @@
 // module that inteprets the keyboard input from the user for use in bomberman.v.
 
 module keyboard_decoder(
-	output reg p1_bomb, p2_bomb,			// 0: bomb NOT placed,					1: bomb placed.
-	output reg p1_xdir, p2_xdir,			// 0: moving LEFT in x direction, 	1: moving RIGHT in x direction.
-	output reg p1_ydir, p2_ydir,			// 0: moving UP in y direction,		1: moving DOWN in y direction.
-	output reg p1_x_moving, p2_x_moving,// 0: NOT moving in x direction, 	1: moving in x direction.
-	output reg p1_y_moving, p2_y_moving,// 0: NOT moving in y direction, 	1: moving in y direction.
+	output reg p1_bomb, p2_bomb,		// 0: bomb NOT placed,					1: bomb placed.
+	output reg p1_xdir, p2_xdir,		// 0: moving LEFT in x direction, 	1: moving RIGHT in x direction.
+	output reg p1_ydir, p2_ydir,		// 0: moving UP in y direction,		1: moving DOWN in y direction.
+	output reg p1_x_mov, p2_x_mov,	// 0: NOT moving in x direction, 	1: moving in x direction.
+	output reg p1_y_mov, p2_y_mov,	// 0: NOT moving in y direction, 	1: moving in y direction.
 	
 	inout PS2_CLK, PS2_DAT,
 	
@@ -38,18 +38,18 @@ module keyboard_decoder(
 	always @(*)
 		begin
 			// for player 1.
-			p1_bomb		<= space;
-			p1_x_dir 	<= d ? d : a;
-			p1_x_moving <= (a | d);
-			p1_y_dir		<= s ? s : w; 
-			p1_y_moving <=	(w | s);
+			p1_bomb	<= space;
+			p1_x_dir <= d ? d : a;
+			p1_x_mov <= (a | d);
+			p1_y_dir	<= s ? s : w; 
+			p1_y_mov <=	(w | s);
 			
 			// for player 2.
-			p2_bomb		<= enter;
-			p2_x_dir 	<= right ? right : left;
-			p2_x_moving <= (left | right);
-			p2_y_dir		<= down ? down : up;
-			p2_y_moving <= (up | down);
+			p2_bomb	<= enter;
+			p2_x_dir <= right ? right : left;
+			p2_x_mov <= (left | right);
+			p2_y_dir	<= down ? down : up;
+			p2_y_mov <= (up | down);
 		end
 
 endmodule
